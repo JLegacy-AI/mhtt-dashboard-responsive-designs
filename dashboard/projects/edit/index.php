e988317a14e37aa900af81dc2ed3fc8d8b2391a6
jlegacy-ai <jamalmuhammad.bsaif20@iba-suk.edu.pk>
2023-10-14 23:14:09 +0500

    Header Message Menu Remove and Setting Updates

--- dashboard/projects/edit/index.php
+++ dashboard/projects/edit/index.php
@@ -38,7 +38,7 @@
     <header id="header" class="header fixed-top d-flex align-items-center">
       <div class="d-flex align-items-center justify-content-between">
           <a href="index.html" class="logo d-flex align-items-center">
-              <img src="../../../assets/img/logo.png" alt="" />
+              <img src="../../assets/img/logo.png" alt="" />
           <span class="d-none d-lg-block">MHTT</span>
         </a>
         <i class="bi bi-list toggle-sidebar-btn"></i>
@@ -59,7 +59,7 @@
           />
           <button type="submit" title="Search">
               <i class="bi bi-search"></i>
-            </button>
+          </button>
         </form>
       </div>
       <!-- End Search Bar -->
@@ -154,95 +154,6 @@
           </li>
           <!-- End Notification Nav -->
 
-          <li class="nav-item dropdown">
-            <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
-              <i class="bi bi-chat-left-text"></i>
-              <span class="badge bg-success badge-number">3</span> </a
-              ><!-- End Messages Icon -->
-              
-              <ul
-              class="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages"
-            >
-              <li class="dropdown-header">
-                You have 3 new messages
-                <a href="#"
-                  ><span class="badge rounded-pill bg-primary p-2 ms-2"
-                  >View all</span
-                  ></a
-                  >
-              </li>
-              <li>
-                  <hr class="dropdown-divider" />
-                </li>
-                
-                <li class="message-item">
-                    <a href="#">
-                        <img
-                        src="../../../assets/img/messages-1.jpg"
-                    alt=""
-                    class="rounded-circle"
-                    />
-                    <div>
-                        <h4>Maria Hudson</h4>
-                    <p>
-                        Velit asperiores et ducimus soluta repudiandae labore
-                      officia est ut...
-                    </p>
-                    <p>4 hrs. ago</p>
-                </div>
-                </a>
-            </li>
-            <li>
-                <hr class="dropdown-divider" />
-            </li>
-            
-            <li class="message-item">
-                <a href="#">
-                  <img
-                  src="../../../assets/img/messages-2.jpg"
-                  alt=""
-                  class="rounded-circle"
-                  />
-                  <div>
-                    <h4>Anna Nelson</h4>
-                    <p>
-                      Velit asperiores et ducimus soluta repudiandae labore
-                      officia est ut...
-                    </p>
-                    <p>6 hrs. ago</p>
-                  </div>
-                </a>
-              </li>
-              <li>
-                  <hr class="dropdown-divider" />
-                </li>
-                
-                <li class="message-item">
-                <a href="#">
-                  <img
-                  src="../../../assets/img/messages-3.jpg"
-                  alt=""
-                  class="rounded-circle"
-                  />
-                  <div>
-                      <h4>David Muldon</h4>
-                      <p>
-                          Velit asperiores et ducimus soluta repudiandae labore
-                          officia est ut...
-                        </p>
-                        <p>8 hrs. ago</p>
-                    </div>
-                </a>
-              </li>
-              <li>
-                  <hr class="dropdown-divider" />
-              </li>
-              
-              <li class="dropdown-footer">
-                <a href="#">Show all messages</a>
-              </li>
-            </ul>
-            <!-- End Messages Dropdown Items -->
         </li>
           <!-- End Messages Nav -->
 
@@ -253,7 +164,7 @@
             data-bs-toggle="dropdown"
             >
             <img
-            src="../../../assets/img/profile-img.jpg"
+            src="../../assets/img/profile-img.jpg"
             alt="Profile"
             class="rounded-circle"
             />
@@ -267,7 +178,6 @@
             >
               <li class="dropdown-header">
                   <h6>Kevin Anderson</h6>
-                <span>Web Designer</span>
             </li>
             <li>
                 <hr class="dropdown-divider" />
@@ -282,38 +192,20 @@
                 <span>My Profile</span>
             </a>
         </li>
-        <li>
+            <li>
                 <hr class="dropdown-divider" />
             </li>
-            
-            <li>
-                <a
-                  class="dropdown-item d-flex align-items-center"
-                  href="users-profile.html"
-                  >
-                  <i class="bi bi-gear"></i>
-                  <span>Account Settings</span>
-                </a>
-            </li>
+
             <li>
                 <hr class="dropdown-divider" />
             </li>
             
-            <li>
-                <a
-                class="dropdown-item d-flex align-items-center"
-                href="pages-faq.html"
-                >
-                <i class="bi bi-question-circle"></i>
-                <span>Need Help?</span>
-            </a>
-        </li>
               <li>
                   <hr class="dropdown-divider" />
                 </li>
                 
                 <li>
-                    <a class="dropdown-item d-flex align-items-center" href="#">
+                    <a class="dropdown-item d-flex align-items-center" href="../../">
                         <i class="bi bi-box-arrow-right"></i>
                   <span>Sign Out</span>
                 </a>
